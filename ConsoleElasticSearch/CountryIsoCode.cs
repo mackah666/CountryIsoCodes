2bb50fb01785ef15d1ef1b5fda696b5a89b8df34
Michael Ackah <mackah666@yahoo.co.uk>
2016-04-22 01:05:52 +0100

    Extended CountryIsoCode class

--- ConsoleElasticSearch/CountryIsoCode.cs
+++ ConsoleElasticSearch/CountryIsoCode.cs
@@ -9,5 +9,10 @@ namespace ConsoleElasticSearch
     class CountryIsoCode
     {
         public string Country { get; set; }
+        public string AlphaTwoCode { get; set; }
+        public string AlphaThreeCode { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+
     }
 }
